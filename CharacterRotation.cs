d38f9b676075c112e8771e0759f449e9f0fc8107
FlakeOrg <flakeorg@gmail.com>
2026-02-02 08:39:02 -0800

    Create CharacterRotation.cs

---
+++ CharacterRotation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRotation : MonoBehavior
+{
+  public Transform playerObj
+  void Start()
+  {
+  // hide the cursor or something
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+  }
+
+  void Update()
+  {
+    
+  }
+
+
+}
