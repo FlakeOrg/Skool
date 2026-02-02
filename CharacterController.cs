b20bad8d6abde65376fa96d6d1865f9c7d332c31
FlakeOrg <flakeorg@gmail.com>
2026-02-02 11:52:42 -0800

    Fix CharacterController class and camera variable names

--- CharacterController.cs
+++ CharacterController.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : Monobehavior
+public class CharacterController : MonoBehavior
 {
   [Header("Refs")]
-  public Transform cam;
-  public Transform cam;
+  public Transform cameraTransform;
+  public Transform player;
   [Header("Speed and other stuff")]
   public float jumpForce = 10f;
+  public float speedDecay = .5f;
 
   void Start()
   {
@@ -19,6 +20,9 @@ public class CharacterController : Monobehavior
   {
     if (Input.GetKey(KeyCode.W))
     {
+      Vector3 cameraForward = camera.forward;
+      cameraForward.y = 0f; // to prevent players from walking on air
+      cameraForward.Normalize()
       
     }
   }
