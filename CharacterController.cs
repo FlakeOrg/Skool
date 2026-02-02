12c230ecfa24d19cc3fc7eab95db44cccb065230
FlakeOrg <flakeorg@gmail.com>
2026-02-02 12:01:56 -0800

    Make... stuff

--- CharacterController.cs
+++ CharacterController.cs
@@ -2,28 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehavior
+public class CharacterController : MonoBehaviour
 {
   [Header("Refs")]
   public Transform cameraTransform;
-  public Transform player;
+  public Rigidbody rb;
   [Header("Speed and other stuff")]
-  public float jumpForce = 10f;
-  public float speedDecay = .5f;
+  public float jumpForce = 3f;
+  public float speed = 10f;
 
   void Start()
   {
 
   }
 
-  void Update()
+  void FixedUpdate()
   {
     if (Input.GetKey(KeyCode.W))
     {
-      Vector3 cameraForward = camera.forward;
+      Vector3 cameraForward = cameraTransform.forward;
       cameraForward.y = 0f; // to prevent players from walking on air
-      cameraForward.Normalize()
-      
+      cameraForward.Normalize();
+
+      rb.AddForce(cameraForward * speed, ForceMode.VelocityChange);
     }
   }
 }
