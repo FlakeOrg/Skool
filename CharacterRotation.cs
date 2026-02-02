abb117bd66a4de2dbcb818ddfc71091c40251e4a
FlakeOrg <flakeorg@gmail.com>
2026-02-02 09:34:55 -0800

    i have no idea what i did

--- CharacterRotation.cs
+++ CharacterRotation.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class CharacterRotation : MonoBehavior
 {
-  public Transform playerObj
+  [Header("references")]
+  public Transform playerObj;
+  public Transform camera;
+  [Header("settings and stuff")]
+  public float rotSpeed = 3f;
   void Start()
   {
   // hide the cursor or something
@@ -15,6 +19,26 @@ public class CharacterRotation : MonoBehavior
   void Update()
   {
     
+  }
+
+  void playerRotation()
+  {
+
+    // in this part (the next 4 ish lines) i honestly just asked chatgpt how to detect it wasd, it said to do this. im suspicious of it but ill do it.
+    
+    float horizontal = Input.GetAxisRaw("Horizontal"); //left and right, A and D
+    float vertical = Input.GetAxisRaw("Vertical"); //forward and backward, W and S
+    
+    Vector3 inputDirection = new Vector3(horizontal, 0f, vertical).normalized;
+
+    if (inputDirection >= 0.1f)
+    {
+      Vector3 cameraForward = camera.forward;
+      cameraForward.y = 0f //this is to prevent my model from going crazy and tilting weirdly when i look up or down
+      cameraForward.Normalize();
+
+      Vector3 moveDir = cameraForward * inputDirection.z + cameraTransform.right * inputDirection.x;
+    }
   }
 
 
