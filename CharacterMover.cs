da90ca47e14b550f4895370533cf48342dac0f57
FlakeOrg <flakeorg@gmail.com>
2026-02-02 14:04:00 -0800

    Add CharacterMover.cs file

--- CharacterMover.cs
+++ CharacterMover.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharacterMover : MonoBehaviour
 {
   [Header("Refs")]
   public Transform cameraTransform;
