ed5c752e35a5b87e0a902af1687fb6292b061060
FlakeOrg <flakeorg@gmail.com>
2026-02-02 14:01:40 -0800

    add a warning

--- CharacterController.cs
+++ CharacterController.cs
@@ -10,20 +10,22 @@ public class CharacterController : MonoBehaviour
   [Header("Speed and other stuff")]
   public float jumpForce = 3f;
   public float speed = 10f;
+  private bool isMovingForward;
 
   void Start()
   {
-
+    isMovingForward = (Input.GetKey(KeyCode.W));
   }
 
   void FixedUpdate()
   {
-    if (Input.GetKey(KeyCode.W))
+    if isMovingForward
     {
       Vector3 cameraForward = cameraTransform.forward;
       cameraForward.y = 0f; // to prevent players from walking on air
       cameraForward.Normalize();
 
+      // i am not joking this will literally turn your player into a ROCKET. the only reason why im leaving this in here is because its just funny. we will change this later.
       rb.AddForce(cameraForward * speed, ForceMode.VelocityChange);
     }
   }
